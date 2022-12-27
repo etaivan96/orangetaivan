b737fb31236e5edc13659d04e0bf7d2d1e1b5382
Morning <morning@mstars-lab3-11s-iMac.local>
2022-12-27 12:20:40 +0800

    E-commerce Web First version

---
+++ indexCon.js
@@ -0,0 +1,9 @@
+let body = document.getElementsByTagName(`body`)[0];
+let main = document.getElementsByTagName(`main`)[0];
+let mainSection = document.getElementsByTagName(`section`)[0];
+let header = document.getElementsByTagName (`header`)[0];
+let headerTop = document.createElement(`div`)
+
+// classList(`top justify-content-between d-flex`).appendChlid(header);
+// let headerTopLeft = document.createElement(``)
+
