6b2de647756161c7cbc8223fd5f5deef1d04b65d
Morning <morning@mstars-lab3-11s-iMac.local>
2022-12-27 12:29:36 +0800

    2nd time

--- indexCon.js
+++ indexCon.js
@@ -4,6 +4,24 @@ let mainSection = document.getElementsByTagName(`section`)[0];
 let header = document.getElementsByTagName (`header`)[0];
 let headerTop = document.createElement(`div`)
 
-// classList(`top justify-content-between d-flex`).appendChlid(header);
-// let headerTopLeft = document.createElement(``)
 
+
+
+
+
+// Adding class in here
+
+headerTop.className += "addCSS";
+var hhhhh = header.appendChild(headerTop);
+
+
+// Adding IDs in here
+
+
+
+
+
+
+
+// Printing the result
+console.log(hhhhh);
